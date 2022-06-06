c476b034f44f3754ac894dd0414ed8d7c2bacd77
DaNny <danniskolobosiky@gmail.com>
2022-06-06 22:58:08 +0100

    I can now creat a venue

--- app.py
+++ app.py
@@ -245,19 +245,30 @@ def show_venue(venue_id):
 @app.route('/venues/create', methods=['GET'])
 def create_venue_form():
   form = VenueForm()
+  
   return render_template('forms/new_venue.html', form=form)
 
 @app.route('/venues/create', methods=['POST'])
 def create_venue_submission():
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
 
-  # on successful db insert, flash success
-  flash('Venue ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
-  return render_template('pages/home.html')
+    error = False
+
+    try:
+        payload = request.get_json()['new_venue']
+        new_venue = Venue(name=payload['name'], city=payload['city'], state=payload['state'], phone=payload['phone'], genres=payload['genres'], facebook_link=payload['facebook_url'], image_link=payload['image_url'], website_link=payload['website_url'], talent_search=payload['talent_search'], description=payload['description'])
+        db.session.add(new_venue)
+        db.session.commit()
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+        if not error:
+            return jsonify(payload)
+        else: 
+            abort(500)
+  
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
 def delete_venue(venue_id):
